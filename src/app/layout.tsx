06d6bbe63f47242b8ef360dfe8175f5280d88d56
Bernaardo <ajbnum@gmail.com>
2024-07-27 01:54:18 -0300

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-     
+     <head><link rel="icon" href="/icon.png" /></head>
       <body>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
